1c1480c9c344ffe49f0b0121354a763aabc42c18
edis <edis.sinanovic@student.um.si>
2024-03-19 10:19:30 +0100

    spring boot init

---
+++ drivings/src/main/java/si/um/feri/ita/vaje/drivings/DrivingsApplication.java
@@ -0,0 +1,13 @@
+package si.um.feri.ita.vaje.drivings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingsApplication.class, args);
+	}
+
+}
---
+++ drivings/src/test/java/si/um/feri/ita/vaje/drivings/DrivingsApplicationTests.java
@@ -0,0 +1,13 @@
+package si.um.feri.ita.vaje.drivings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
